6af2e76ca5a00b9f61841379edece949f881b44b
Predrag Rakic <prakic@renderedtext.com>
2017-07-25 11:30:38 +0200

    refactor Dockerfile.eex

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Usvc.Mixfile do
 
   def project do
     [app: :usvc,
-     version: "1.7.0",
+     version: "1.8.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
