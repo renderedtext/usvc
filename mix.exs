594f9b08ccc71ab8f9737fa4d3bd47b74b850137
Zorica Suvajdzin Rakic <zorica.suvajdzin@gmail.com>
2017-08-01 02:26:55 +0200

    Adjust usvc with v2 renderedtext/elixir image

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Usvc.Mixfile do
 
   def project do
     [app: :usvc,
-     version: "1.8.3",
+     version: "1.9.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
