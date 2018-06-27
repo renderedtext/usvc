ad93d224f5ae301fba566f92c5811aef4ac36357
Predrag Rakic <prakic@renderedtext.com>
2018-06-27 11:40:26 +0200

    use Elixir 1.6

--- priv/templates/default/mix.exs
+++ priv/templates/default/mix.exs
@@ -6,8 +6,8 @@ defmodule <%= prj_module_name %>.Mixfile do
 
   def project do
     [app: <%= prj_atom %>,
-     version: "0.1.0",
-     elixir: "~> 1.4",
+     version: "0.2.0",
+     elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -42,4 +42,3 @@ defmodule <%= prj_module_name %>.Mixfile do
     ]
   end
 end
-
