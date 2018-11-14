d42b6278024fd5ef47544cc76dfcecb1d609c6b6
Predrag Rakic <pec@bekalica>
2018-11-14 16:29:02 +0100

    distillery release with ecto migration

--- priv/templates/default/mix.exs
+++ priv/templates/default/mix.exs
@@ -33,12 +33,13 @@ defmodule <%= prj_module_name %>.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:cowboy, "~> 1.1.0"},
       {:plug, "~> 1.3"},
-#      {:poison, "~> 3.1"},
+      {:plug_cowboy, "~> 1.0"},
       {:httpoison, "~> 0.11"},
+      {:distillery, "~> 2.0"},
+      {:postgrex, "~> 0.13"},
+      {:ecto, "~> 2.2"},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
-      {:mix_test_watch, "~> 0.3", only: :dev, runtime: false},
     ]
   end
 end
